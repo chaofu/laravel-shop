f5d5d6aab409f153e2ef4efbaa2ae58dabd7f962
vagrant <vagrant@homestead>
2020-02-16 10:16:45 +0000

    管理后台 -新增，编辑商品

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,12 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
+//        'public' => [
+//            'driver' => 'local',
+//            'root' => public_path('admin_img'),
+//            'url' => env('APP_URL').'/admin_img',
+//            'visibility' => 'public',
+//        ],
 
         's3' => [
             'driver' => 's3',
