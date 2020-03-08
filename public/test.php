9acf5fd2c83d78120b583b1ec3bfb7481e4be50a
vagrant <vagrant@homestead>
2020-03-08 01:43:54 +0000

    订单模型

--- public/test.php
+++ public/test.php
@@ -1,15 +1,17 @@
 <?php
-$stime=microtime(true); //获取程序开始执行的时间
+//$stime=microtime(true); //获取程序开始执行的时间
+//
+///**
+// *你要运行的程序
+// **/
+//$a = 0;
+//for($i=0;$i<5000000;$i++){
+//    $a=$a*$i;
+//}
+//
+//
+//$etime=microtime(true);//获取程序执行结束的时间
+//$total=$etime-$stime;   //计算差值
+//echo "<br />[页面执行时间：{$total} ]秒";
 
-/**
- *你要运行的程序
- **/
-$a = 0;
-for($i=0;$i<5000000;$i++){
-    $a=$a*$i;
-}
-
-
-$etime=microtime(true);//获取程序执行结束的时间
-$total=$etime-$stime;   //计算差值
-echo "<br />[页面执行时间：{$total} ]秒";
+phpinfo();
